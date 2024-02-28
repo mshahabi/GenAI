0f67655c9603ed1ed4167ba217750c9485de388e
Mehrdad Shahabi <mmsh@mehrdads-mbp.mynetworksettings.com>
2024-02-27 22:16:44 -0500

    transformers added

--- src/test.py
+++ src/test.py
@@ -1,3 +1,18 @@
-import pandas as pd 
+import torch
+import math
 
-print("This is a testfile after many years")
+print(torch.backends.mps.is_available()) #the MacOS is higher than 12.3+
+print(torch.backends.mps.is_built()) #MPS is activated
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import time
+
+# Check if GPU is available
+# device = torch.device("mps" if torch.cuda.is_available() else "cpu")
+device = torch.device("mps")
+# device = torch.device("cpu")
+
+print(device)
