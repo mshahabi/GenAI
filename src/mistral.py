4a8fa6a424433f0f00b4633d41e8eb219c703dee
Mehrdad Shahabi <mmsh@mehrdads-mbp.mynetworksettings.com>
2024-02-29 22:53:25 -0500

    mls

--- src/mistral.py
+++ src/mistral.py
@@ -7,3 +7,5 @@ from mlx_lm import load, generate
 model, tokenizer = load("mistralai/Mistral-7B-Instruct-v0.2")
 
 response = generate(model, tokenizer, prompt="hello", verbose=True)
+
+print(response)
